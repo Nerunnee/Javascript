f50c8e4476aca0fbddaf5cb660c4c934a586c781
norovsuren15 <e.norvoo@gmail.com>
2025-11-17 12:57:42 +0800

    index.html

---
+++ script.js
@@ -0,0 +1,81 @@
+console.log("Hello world");
+
+// Бодлого 1.
+let x = 5;
+
+let y = 3;
+
+let sum = x + y;
+
+console.log("Бодлого 1", sum);
+
+// Бодлого 2.
+let a = 10;
+
+let b = 4;
+
+sum = a - b;
+
+console.log("Бодлого 2", sum);
+
+// Бодлого 3.
+let p = 7;
+
+const q = 6;
+
+sum = p * q;
+
+console.log("Бодлого 3", sum);
+
+// Бодлого 4.
+const m = 20;
+
+const n = 5;
+
+sum = p * q;
+
+console.log("Бодлого 4", sum);
+
+// Бодлого 5.
+x = 15;
+
+y = 4;
+
+sum = x % y;
+
+console.log("Бодлого 5", sum);
+
+// Бодлого 6.
+let num = 10;
+
+num += 5;
+
+console.log("Бодлого 6", num);
+
+// Бодлого 7.
+num = 20;
+
+num += 10;
+
+console.log("Бодлого 7", num);
+
+// Бодлого 8.
+num = 50;
+
+num -= 20;
+
+console.log("Бодлого 8", num);
+
+// Бодлого 9.
+num = 6;
+
+num *= 3;
+
+console.log("Бодлого 9", num);
+
+// Бодлого 10.
+num = 16;
+
+num /= 4;
+
+console.log("Бодлого 10", num);
---
+++ script2.js
@@ -0,0 +1,57 @@
+// 2-р бодлогын хэсэг
+
+// Бодлого 2.1.
+
+num = 12;
+num *= 3;
+
+console.log("Бодлого 2.1", num);
+
+// Бодлого 2.2.
+
+x = 1;
+y = 3;
+a = 5;
+b = 7;
+p = 8.7;
+
+sum = x + y + n + m + p;
+
+console.log("Бодлого 2.2", sum);
+
+// Бодлого 2.3.
+
+x = 10;
+y = 5;
+
+let s = x * y;
+p = 2 * (x + y);
+
+console.log("Бодлого 2.3", s, p);
+
+// Бодлого 2.4.
+
+let sec = 98;
+let min = sec / 60;
+console.log(Math.floor(min), "min");
+let uldegdelSecund = sec % 60;
+console.log(uldegdelSecund, "sec");
+
+console.log(
+  "Бодлого 2.4",
+  `${Math.floor(min)} мин : ${uldegdelSecund} секунд :`
+);
+
+// Бодлого 2.5.
+
+sec = 1297;
+min = sec / 60;
+uldegdelSecund = sec % 60;
+let realMin = (sec - uldegdelSecund) / 60;
+let uldegdelMinut = realMin % 60;
+let hour = (realMin - uldegdelMin) / 60;
+
+console.log(
+  "Бодлого 2.5",
+  `${hour} цаг : ${Math.floor(uldegdelMinut)} мин : ${uldegdelSecund} секунд :`
+);
